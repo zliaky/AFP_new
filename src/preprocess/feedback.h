2edc2d1e335110b50fefe2023cfbd78169d5041c
xuweijie <xuweijie1995@sina.cn>
2016-06-04 19:20:33 +0800

    preprocess

---
+++ src/preprocess/feedback.h
@@ -0,0 +1,16 @@
+#ifndef __FEEDBACK__
+#define __FEEDBACK__
+
+#include "defines.h"
+
+class Feedback {
+public:
+	int imp_time;
+	long long pos_id;
+	int click_num;
+
+	int imp_time_map;
+	int pos_id_map;
+};
+
+#endif
